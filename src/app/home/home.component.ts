c058fa9f82e63b5551e165b49a7a20900af25983
Gulfam Ali <aligulfam6@gmail.com>
2018-01-30 23:07:23 +0530

    Settings Added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,12 @@ export class HomeComponent implements OnInit {
   total_records: string[];
   validate: string[];
   posts = [];
+  loading_post = true;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
      this.http.get('http://localhost/riter/api/guestfeed').subscribe(res => {
+          this.loading_post = false;
           this.validate = res['validate'];
           this.total_records = res['total_records'];
           this.posts = res['data'];
